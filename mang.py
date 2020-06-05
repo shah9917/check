1a96dfd0c0de82725191a1128ac8a48eb4fc9344
hshah917 <39160648+shah9917@users.noreply.github.com>
2020-06-05 11:47:36 +0545

    Started python project

---
+++ mang.py
@@ -0,0 +1,11 @@
+tickets = {'concert': 500, 'workshop': 700, 'seminar': '300'}
+while True:
+    buy = int(input('Which ticket to buy:1-concert,2-workshop,3-seminar'))
+    if buy==1:
+        print('Here is your concert ticket. Enjoy!')
+    elif buy==2:
+        print('Here is your workshop ticket.Enjoy!')
+    elif buy==3:
+        print("Here is your seminar ticket. Enjoy!")
+    else:
+        print('Invalid Selection')
