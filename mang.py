98645bde7ebb88843fde716ab7d8aafa0e1df528
shah9917 <39160648+shah9917@users.noreply.github.com>
2020-06-05 11:53:55 +0545

    Update mang.py

--- mang.py
+++ mang.py
@@ -1,6 +1,7 @@
 tickets = {'concert': 500, 'workshop': 700, 'seminar': '300'}
 while True:
     buy = int(input('Which ticket to buy:1-concert,2-workshop,3-seminar'))
+    q = int(input("How many tickets would you like to buy?"))
     if buy==1:
         print('Here is your concert ticket. Enjoy!')
     elif buy==2:
